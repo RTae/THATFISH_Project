83df8dacf54ff28b34bf412761a78541fc1610ed
RTae <potae02@gmail.com>
2020-03-17 22:09:29 +0700

    delAuth

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,12 +2,11 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeStack from './homeStack'
-import AuthStack from './AuthStack'
 
 export default function App() {
   return (
     <NavigationContainer>
-      <AuthStack/>
+      <HomeStack/>
     </NavigationContainer>
   );
 }
